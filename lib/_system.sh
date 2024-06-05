4b1de4af805d61469fa6cf5ab39950ff8fc4965a
thiagotxt <txtoperadora@gmail.com>
2024-06-05 03:00:48 -0300

    Update _system.sh

--- lib/_system.sh
+++ lib/_system.sh
@@ -14,10 +14,10 @@ system_create_user() {
 
   sleep 2
 
-  sudo su - root <<EOF
-  useradd -m -p $(openssl passwd -crypt ${mysql_root_password}) -s /bin/bash -G sudo deploy
-  usermod -aG sudo deploy
-EOF
+  su - root -c "
+    useradd -m -p $(openssl passwd -crypt ${mysql_root_password}) -s /bin/bash -G sudo deploy
+    usermod -aG sudo deploy
+  "
 
   sleep 2
 }
@@ -32,12 +32,11 @@ system_git_clone() {
   printf "${WHITE} 💻 Fazendo download do código whaticket...${GRAY_LIGHT}"
   printf "\n\n"
 
-
   sleep 2
 
-  sudo su - deploy <<EOF
-  git clone ${link_git} /home/deploy/${instancia_add}/
-EOF
+  su - deploy -c "
+    git clone ${link_git} /home/deploy/${instancia_add}/
+  "
 
   sleep 2
 }
@@ -54,15 +53,13 @@ system_update() {
 
   sleep 2
 
-  sudo su - root <<EOF
-  apt -y update
-  sudo apt-get install -y libxshmfence-dev libgbm-dev wget unzip fontconfig locales gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils
-EOF
+  su - root -c "
+    apt -y update
+    apt-get install -y libxshmfence-dev libgbm-dev wget unzip fontconfig locales gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils
+  "
 
   sleep 2
 }
-
-
 
 #######################################
 # delete system
@@ -76,28 +73,26 @@ deletar_tudo() {
 
   sleep 2
 
-  sudo su - root <<EOF
-  docker container rm redis-${empresa_delete} --force
-  cd && rm -rf /etc/nginx/sites-enabled/${empresa_delete}-frontend
-  cd && rm -rf /etc/nginx/sites-enabled/${empresa_delete}-backend  
-  cd && rm -rf /etc/nginx/sites-available/${empresa_delete}-frontend
-  cd && rm -rf /etc/nginx/sites-available/${empresa_delete}-backend
-  
+  su - root -c "
+    docker container rm redis-${empresa_delete} --force
+    rm -rf /etc/nginx/sites-enabled/${empresa_delete}-frontend
+    rm -rf /etc/nginx/sites-enabled/${empresa_delete}-backend  
+    rm -rf /etc/nginx/sites-available/${empresa_delete}-frontend
+    rm -rf /etc/nginx/sites-available/${empresa_delete}-backend
+  "
+
   sleep 2
 
-  sudo su - postgres
-  dropuser ${empresa_delete}
-  dropdb ${empresa_delete}
-  exit
-EOF
+  su - postgres -c "
+    dropuser ${empresa_delete}
+    dropdb ${empresa_delete}
+  "
 
-sleep 2
-
-sudo su - deploy <<EOF
- rm -rf /home/deploy/${empresa_delete}
- pm2 delete ${empresa_delete}-frontend ${empresa_delete}-backend
- pm2 save
-EOF
+  su - deploy -c "
+    rm -rf /home/deploy/${empresa_delete}
+    pm2 delete ${empresa_delete}-frontend ${empresa_delete}-backend
+    pm2 save
+  "
 
   sleep 2
 
@@ -105,9 +100,7 @@ EOF
   printf "${WHITE} 💻 Remoção da Instancia/Empresa ${empresa_delete} realizado com sucesso ...${GRAY_LIGHT}"
   printf "\n\n"
 
-
   sleep 2
-
 }
 
 #######################################
@@ -122,10 +115,10 @@ configurar_bloqueio() {
 
   sleep 2
 
-sudo su - deploy <<EOF
- pm2 stop ${empresa_bloquear}-backend
- pm2 save
-EOF
+  su - deploy -c "
+    pm2 stop ${empresa_bloquear}-backend
+    pm2 save
+  "
 
   sleep 2
 
@@ -135,7 +128,6 @@ EOF
 
   sleep 2
 }
-
 
 #######################################
 # desbloquear system
@@ -149,10 +141,10 @@ configurar_desbloqueio() {
 
   sleep 2
 
-sudo su - deploy <<EOF
- pm2 start ${empresa_bloquear}-backend
- pm2 save
-EOF
+  su - deploy -c "
+    pm2 start ${empresa_bloquear}-backend
+    pm2 save
+  "
 
   sleep 2
 
@@ -173,31 +165,31 @@ configurar_dominio() {
   printf "${WHITE} 💻 Vamos Alterar os Dominios do whaticket...${GRAY_LIGHT}"
   printf "\n\n"
 
-sleep 2
+  sleep 2
 
-  sudo su - root <<EOF
-  cd && rm -rf /etc/nginx/sites-enabled/${empresa_dominio}-frontend
-  cd && rm -rf /etc/nginx/sites-enabled/${empresa_dominio}-backend  
-  cd && rm -rf /etc/nginx/sites-available/${empresa_dominio}-frontend
-  cd && rm -rf /etc/nginx/sites-available/${empresa_dominio}-backend
-EOF
+  su - root -c "
+    rm -rf /etc/nginx/sites-enabled/${empresa_dominio}-frontend
+    rm -rf /etc/nginx/sites-enabled/${empresa_dominio}-backend  
+    rm -rf /etc/nginx/sites-available/${empresa_dominio}-frontend
+    rm -rf /etc/nginx/sites-available/${empresa_dominio}-backend
+  "
 
-sleep 2
+  sleep 2
 
-  sudo su - deploy <<EOF
-  cd && cd /home/deploy/${empresa_dominio}/frontend
-  sed -i "1c\REACT_APP_BACKEND_URL=https://${alter_backend_url}" .env
-  cd && cd /home/deploy/${empresa_dominio}/backend
-  sed -i "2c\BACKEND_URL=https://${alter_backend_url}" .env
-  sed -i "3c\FRONTEND_URL=https://${alter_frontend_url}" .env 
-EOF
+  su - deploy -c "
+    cd /home/deploy/${empresa_dominio}/frontend
+    sed -i '1c\REACT_APP_BACKEND_URL=https://${alter_backend_url}' .env
+    cd /home/deploy/${empresa_dominio}/backend
+    sed -i '2c\BACKEND_URL=https://${alter_backend_url}' .env
+    sed -i '3c\FRONTEND_URL=https://${alter_frontend_url}' .env 
+  "
 
-sleep 2
+  sleep 2
    
-   backend_hostname=$(echo "${alter_backend_url/https:\/\/}")
+  backend_hostname=$(echo "${alter_backend_url/https:\/\/}")
 
- sudo su - root <<EOF
-  cat > /etc/nginx/sites-available/${empresa_dominio}-backend << 'END'
+  su - root -c "
+    cat > /etc/nginx/sites-available/${empresa_dominio}-backend << END
 server {
   server_name $backend_hostname;
   location / {
@@ -213,15 +205,15 @@ server {
   }
 }
 END
-ln -s /etc/nginx/sites-available/${empresa_dominio}-backend /etc/nginx/sites-enabled
-EOF
+    ln -s /etc/nginx/sites-available/${empresa_dominio}-backend /etc/nginx/sites-enabled
+  "
 
-sleep 2
+  sleep 2
 
-frontend_hostname=$(echo "${alter_frontend_url/https:\/\/}")
+  frontend_hostname=$(echo "${alter_frontend_url/https:\/\/}")
 
-sudo su - root << EOF
-cat > /etc/nginx/sites-available/${empresa_dominio}-frontend << 'END'
+  su - root -c "
+    cat > /etc/nginx/sites-available/${empresa_dominio}-frontend << END
 server {
   server_name $frontend_hostname;
   location / {
@@ -237,27 +229,27 @@ server {
   }
 }
 END
-ln -s /etc/nginx/sites-available/${empresa_dominio}-frontend /etc/nginx/sites-enabled
-EOF
+    ln -s /etc/nginx/sites-available/${empresa_dominio}-frontend /etc/nginx/sites-enabled
+  "
 
- sleep 2
+  sleep 2
 
- sudo su - root <<EOF
-  service nginx restart
-EOF
+  su - root -c "
+    service nginx restart
+  "
 
   sleep 2
 
   backend_domain=$(echo "${backend_url/https:\/\/}")
   frontend_domain=$(echo "${frontend_url/https:\/\/}")
 
-  sudo su - root <<EOF
-  certbot -m $deploy_email \
-          --nginx \
-          --agree-tos \
-          --non-interactive \
-          --domains $backend_domain,$frontend_domain
-EOF
+  su - root -c "
+    certbot -m $deploy_email \
+            --nginx \
+            --agree-tos \
+            --non-interactive \
+            --domains $backend_domain,$frontend_domain
+  "
 
   sleep 2
 
@@ -280,22 +272,22 @@ system_node_install() {
 
   sleep 2
 
-  sudo su - root <<EOF
-  curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-  apt-get install -y nodejs
-  sleep 2
-  npm install -g npm@latest
-  sleep 2
-  sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-  wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-  sudo apt-get update -y && sudo apt-get -y install postgresql
-  sleep 2
-  sudo timedatectl set-timezone America/Sao_Paulo
-  
-EOF
+  su - root -c "
+    curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
+    apt-get install -y nodejs
+    sleep 2
+    npm install -g npm@latest
+    sleep 2
+    echo 'deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main' > /etc/apt/sources.list.d/pgdg.list
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+    apt-get update -y && apt-get -y install postgresql
+    sleep 2
+    timedatectl set-timezone America/Sao_Paulo
+  "
 
   sleep 2
 }
+
 #######################################
 # installs docker
 # Arguments:
@@ -308,17 +300,17 @@ system_docker_install() {
 
   sleep 2
 
-  sudo su - root <<EOF
-  apt install -y apt-transport-https \
-                 ca-certificates curl \
-                 software-properties-common
+  su - root -c "
+    apt install -y apt-transport-https \
+                   ca-certificates curl \
+                   software-properties-common
 
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-  
-  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+    
+    add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable'
 
-  apt install -y docker-ce
-EOF
+    apt install -y docker-ce
+  "
 
   sleep 2
 }
@@ -341,51 +333,51 @@ system_puppeteer_dependencies() {
 
   sleep 2
 
-  sudo su - root <<EOF
-  apt-get install -y libxshmfence-dev \
-                      libgbm-dev \
-                      wget \
-                      unzip \
-                      fontconfig \
-                      locales \
-                      gconf-service \
-                      libasound2 \
-                      libatk1.0-0 \
-                      libc6 \
-                      libcairo2 \
-                      libcups2 \
-                      libdbus-1-3 \
-                      libexpat1 \
-                      libfontconfig1 \
-                      libgcc1 \
-                      libgconf-2-4 \
-                      libgdk-pixbuf2.0-0 \
-                      libglib2.0-0 \
-                      libgtk-3-0 \
-                      libnspr4 \
-                      libpango-1.0-0 \
-                      libpangocairo-1.0-0 \
-                      libstdc++6 \
-                      libx11-6 \
-                      libx11-xcb1 \
-                      libxcb1 \
-                      libxcomposite1 \
-                      libxcursor1 \
-                      libxdamage1 \
-                      libxext6 \
-                      libxfixes3 \
-                      libxi6 \
-                      libxrandr2 \
-                      libxrender1 \
-                      libxss1 \
-                      libxtst6 \
-                      ca-certificates \
-                      fonts-liberation \
-                      libappindicator1 \
-                      libnss3 \
-                      lsb-release \
-                      xdg-utils
-EOF
+  su - root -c "
+    apt-get install -y libxshmfence-dev \
+                        libgbm-dev \
+                        wget \
+                        unzip \
+                        fontconfig \
+                        locales \
+                        gconf-service \
+                        libasound2 \
+                        libatk1.0-0 \
+                        libc6 \
+                        libcairo2 \
+                        libcups2 \
+                        libdbus-1-3 \
+                        libexpat1 \
+                        libfontconfig1 \
+                        libgcc1 \
+                        libgconf-2-4 \
+                        libgdk-pixbuf2.0-0 \
+                        libglib2.0-0 \
+                        libgtk-3-0 \
+                        libnspr4 \
+                        libpango-1.0-0 \
+                        libpangocairo-1.0-0 \
+                        libstdc++6 \
+                        libx11-6 \
+                        libx11-xcb1 \
+                        libxcb1 \
+                        libxcomposite1 \
+                        libxcursor1 \
+                        libxdamage1 \
+                        libxext6 \
+                        libxfixes3 \
+                        libxi6 \
+                        libxrandr2 \
+                        libxrender1 \
+                        libxss1 \
+                        libxtst6 \
+                        ca-certificates \
+                        fonts-liberation \
+                        libappindicator1 \
+                        libnss3 \
+                        lsb-release \
+                        xdg-utils
+  "
 
   sleep 2
 }
@@ -402,10 +394,9 @@ system_pm2_install() {
 
   sleep 2
 
-  sudo su - root <<EOF
-  npm install -g pm2
-
-EOF
+  su - root -c "
+    npm install -g pm2
+  "
 
   sleep 2
 }
@@ -422,11 +413,11 @@ system_snapd_install() {
 
   sleep 2
 
-  sudo su - root <<EOF
-  apt install -y snapd
-  snap install core
-  snap refresh core
-EOF
+  su - root -c "
+    apt install -y snapd
+    snap install core
+    snap refresh core
+  "
 
   sleep 2
 }
@@ -443,11 +434,11 @@ system_certbot_install() {
 
   sleep 2
 
-  sudo su - root <<EOF
-  apt-get remove certbot
-  snap install --classic certbot
-  ln -s /snap/bin/certbot /usr/bin/certbot
-EOF
+  su - root -c "
+    apt-get remove certbot
+    snap install --classic certbot
+    ln -s /snap/bin/certbot /usr/bin/certbot
+  "
 
   sleep 2
 }
@@ -464,10 +455,10 @@ system_nginx_install() {
 
   sleep 2
 
-  sudo su - root <<EOF
-  apt install -y nginx
-  rm /etc/nginx/sites-enabled/default
-EOF
+  su - root -c "
+    apt install -y nginx
+    rm /etc/nginx/sites-enabled/default
+  "
 
   sleep 2
 }
@@ -484,9 +475,9 @@ system_nginx_restart() {
 
   sleep 2
 
-  sudo su - root <<EOF
-  service nginx restart
-EOF
+  su - root -c "
+    service nginx restart
+  "
 
   sleep 2
 }
@@ -503,13 +494,11 @@ system_nginx_conf() {
 
   sleep 2
 
-sudo su - root << EOF
-
-cat > /etc/nginx/conf.d/deploy.conf << 'END'
+  su - root -c "
+    cat > /etc/nginx/conf.d/deploy.conf << 'END'
 client_max_body_size 100M;
 END
-
-EOF
+  "
 
   sleep 2
 }
@@ -529,14 +518,13 @@ system_certbot_setup() {
   backend_domain=$(echo "${backend_url/https:\/\/}")
   frontend_domain=$(echo "${frontend_url/https:\/\/}")
 
-  sudo su - root <<EOF
-  certbot -m $deploy_email \
-          --nginx \
-          --agree-tos \
-          --non-interactive \
-          --domains $backend_domain,$frontend_domain
-
-EOF
+  su - root -c "
+    certbot -m $deploy_email \
+            --nginx \
+            --agree-tos \
+            --non-interactive \
+            --domains $backend_domain,$frontend_domain
+  "
 
   sleep 2
 }
